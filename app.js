6ecd3d14d70eae0b2e8fc3cdf09bed3c4c946264
Shanto <rakibulhasansanto25@gmail.com>
2021-03-07 01:57:22 +0600

    Finish  All Project

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
+ var s = $("#result");
 function insertnum(number){
-  var existingNumber = $("#result").val();
+  var existingNumber = s.val();
   $("#result").val(existingNumber + number)
 }
 
 function clearRes(){
-  $("#result").val('')
+  s.val('')
 }
+
+function calculate(){
+   var result = eval(s.val())
+   s.val(result)
+
+}
+
+ function deletNumber(){
+   var presentNumber = s.val();
+   if(presentNumber!=''){
+     s.val(presentNumber.slice(0,-1));
+   }
+ }
